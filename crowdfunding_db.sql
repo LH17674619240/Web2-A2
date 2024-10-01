e12b23e5d72944625334bce26eaae5c1168d20d4
LH17674619240 <806934858@qq.com>
2024-10-01 14:05:21 +0800

    Create a table

---
+++ crowdfunding_db.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: crowdfunding_db
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) NOT NULL,
+  PRIMARY KEY (`CATEGORY_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Health'),(2,'Education'),(3,'Environment'),(4,'Animal Welfare'),(5,'Academic Research');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fundraiser`
+--
+
+DROP TABLE IF EXISTS `fundraiser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fundraiser` (
+  `FUNDRAISER_ID` int NOT NULL AUTO_INCREMENT,
+  `ORGANIZER` varchar(255) NOT NULL,
+  `CAPTION` text NOT NULL,
+  `TARGET_FUNDING` decimal(8,1) NOT NULL,
+  `CURRENT_FUNDING` decimal(8,1) DEFAULT '0.0',
+  `CITY` varchar(255) DEFAULT NULL,
+  `ACTIVE` tinyint(1) DEFAULT '1',
+  `CATEGORY_ID` int DEFAULT NULL,
+  PRIMARY KEY (`FUNDRAISER_ID`),
+  KEY `CATEGORY_ID` (`CATEGORY_ID`),
+  CONSTRAINT `fundraiser_ibfk_1` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fundraiser`
+--
+
+LOCK TABLES `fundraiser` WRITE;
+/*!40000 ALTER TABLE `fundraiser` DISABLE KEYS */;
+INSERT INTO `fundraiser` VALUES (1,'Steve','Blood Cancer Treatment',50000.0,8000.0,'New York',1,1),(2,'Bright Future','Tuition for Poor Students',7000.0,3500.0,'Beijing',1,2),(3,'Clean Ocean Initiative','Beach Cleanup and Trash Removal',8000.0,4500.0,'Miami',1,3),(4,'Save the Yangtze River Porpoise','Save and Protect Endangered Animals',20000.0,5000.0,'Wuhan',1,3),(5,'Heart to Heart','Life-saving Surgery for Children',12000.0,5000.0,'Boston',1,1),(6,'Education Care','Scholarships for Aspiring Engineers',5000.0,3000.0,'Denver',1,2),(7,'Green Future','Install solar panels for local community centers',9000.0,4000.0,'Portland',1,3),(8,'Hope for the Homeless','Provide shelter and meals for the homeless',8000.0,3500.0,'Atlanta',1,1),(9,'Academic Support','Provide assistance to laboratories lacking funds',6000.0,2500.0,'Los Angeles',1,2),(10,'Ancient City Protection','Renovate ancient buildings',30000.0,2000.0,'Phoenix Ancient City',1,3);
+/*!40000 ALTER TABLE `fundraiser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-01 14:04:50
